5e40f38e6bad0cf6bbcc8d1d25d71c736b4a2c43
bhavik <bhavikvashi1804@gmail.com>
2024-06-02 19:10:01 +0530

    Spring MVC: Use-case of ModelAndView

--- todo/src/main/java/com/bhavik/todo/controller/CalculatorController.java
+++ todo/src/main/java/com/bhavik/todo/controller/CalculatorController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("calculator")
@@ -19,8 +20,10 @@ public class CalculatorController {
 
 
     @GetMapping("/add-num")
-    public String add(@RequestParam Long number1, @RequestParam Long number2, Model model){
-        model.addAttribute("result", number1+ number2);
-        return "result";
+    public ModelAndView add(@RequestParam Long number1, @RequestParam Long number2){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("result");
+        modelAndView.addObject("result", number1 + number2);
+        return modelAndView;
     }
 }
